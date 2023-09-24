b363e09ed82dc831952c5087d9887d593726ef63
litonp <litondas7121@gmail.com>
2023-09-24 19:14:02 +0600

    com

--- revisions.js
+++ revisions.js
@@ -2,7 +2,42 @@ var onionPrice = 70;
 var eggPrice = 13;
 // addition , subtraction
 var totalPrice = onionPrice + eggPrice;
-console.log(totalPrice);
+// console.log(totalPrice);
 
 var priceDefference = onionPrice - eggPrice;
-console.log(priceDefference);
+// console.log(priceDefference);
+
+// multiplication
+var orangePrice = 20;
+var quantity = 7;
+var totalPrice = orangePrice * quantity;
+// console.log(totalPrice);
+
+// division
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+// console.log(eachPlayer);
+
+var money = 1000;
+var apple = 300;
+var orange = 400;
+var totalPrice = apple + orange;
+// console.log(totalPrice);
+var remainder = money - totalPrice;
+// console.log(remainder);
+
+var num1 = 75.25;
+var num2 = 80;
+var num3 = 65;
+var num4 = 35.45;
+var num5 = 99.50;
+var num6 = 5;
+
+var totalNum = num1 + num2 + num3 + num4 + num5;
+totalNum /= 5;
+totalNum = totalNum.toFixed(2);
+totalNum = parseFloat(totalNum);
+console.log(totalNum);
+
+
