e82a28def8e1cb19c6391927ffa50895a07892ea
litonp <litondas7121@gmail.com>
2023-09-25 01:00:43 +0600

    icoce

---
+++ fractic.js
@@ -0,0 +1,24 @@
+// var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notbook'];
+// for (var i = 0; i < items.length; i++) {
+//     var item = items[i]
+//     if (item == 'pen') {
+//         break;
+//     }
+//     console.log(item);
+// }
+
+// var numbers = [45, 78, 87, 89, 120, 69, 56, 76];
+// for (var i = 0; i < numbers.length; i++) {
+//     var number = numbers[i]
+//     if (number < 60) {
+//         continue;
+//     }
+//     console.log(number);
+// }
+
+var side1 = 9;
+var side2 = 8;
+var side3 = 9;
+if (side1 == side2 || side1 == side3 || side2 == side3) {
+    console.log('icoceles');
+}
