2bd5053f445189393cb418583444811c4e13a689
litonp <litondas7121@gmail.com>
2023-09-24 18:58:46 +0600

    revision

---
+++ revisions.js
@@ -0,0 +1,8 @@
+var onionPrice = 70;
+var eggPrice = 13;
+// addition , subtraction
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+var priceDefference = onionPrice - eggPrice;
+console.log(priceDefference);
